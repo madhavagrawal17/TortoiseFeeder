9ba571f8d658f6b58768fbaccc057502ccdba0df
Madhav Agrawal <maagr@microsoft.com>
2021-10-13 14:21:13 -0700

    Motor change

--- src/platform.ts
+++ src/platform.ts
@@ -1,14 +1,14 @@
 import { API, DynamicPlatformPlugin, Logger, PlatformAccessory, PlatformConfig, Service, Characteristic } from 'homebridge';
 
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings';
-import { ExamplePlatformAccessory } from './platformAccessory';
+import { ServoStatelessProgrammableSwitchAccessory } from './platformAccessory';
 
 /**
  * HomebridgePlatform
  * This class is the main constructor for your plugin, this is where you should
  * parse the user config and discover/register accessories with Homebridge.
  */
-export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
+export class RaspberryPiHomebridgePlatform implements DynamicPlatformPlugin {
   public readonly Service: typeof Service = this.api.hap.Service;
   public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
 
@@ -19,6 +19,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     public readonly log: Logger,
     public readonly config: PlatformConfig,
     public readonly api: API,
+    private readonly accessory: PlatformAccessory
   ) {
     this.log.debug('Finished initializing platform:', this.config.name);
 
@@ -54,24 +55,20 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
     // EXAMPLE ONLY
     // A real plugin you would discover accessories from the local network, cloud services
     // or a user-defined array in the platform config.
-    const exampleDevices = [
+    const Devices = [
       {
-        exampleUniqueId: 'ABCD',
-        exampleDisplayName: 'Bedroom',
-      },
-      {
-        exampleUniqueId: 'EFGH',
-        exampleDisplayName: 'Kitchen',
-      },
+        UniqueId: 'servo',
+        DisplayName: 'Pet Feeder',
+      }
     ];
 
     // loop over the discovered devices and register each one if it has not already been registered
-    for (const device of exampleDevices) {
+    for (const device of Devices) {
 
       // generate a unique id for the accessory this should be generated from
       // something globally unique, but constant, for example, the device serial
       // number or MAC address
-      const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+      const uuid = this.api.hap.uuid.generate(device.UniqueId);
 
       // see if an accessory with the same uuid has already been registered and restored from
       // the cached devices we stored in the `configureAccessory` method above
@@ -87,7 +84,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the restored accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, existingAccessory);
+        new ServoStatelessProgrammableSwitchAccessory(this, existingAccessory);
 
         // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
         // remove platform accessories when no longer present
@@ -95,10 +92,10 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
         // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
       } else {
         // the accessory does not yet exist, so we need to create it
-        this.log.info('Adding new accessory:', device.exampleDisplayName);
+        this.log.info('Adding new accessory:', device.DisplayName);
 
         // create a new accessory
-        const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+        const accessory = new this.api.platformAccessory(device.DisplayName, uuid);
 
         // store a copy of the device object in the `accessory.context`
         // the `context` property can be used to store any data about the accessory you may need
@@ -106,7 +103,7 @@ export class ExampleHomebridgePlatform implements DynamicPlatformPlugin {
 
         // create the accessory handler for the newly create accessory
         // this is imported from `platformAccessory.ts`
-        new ExamplePlatformAccessory(this, accessory);
+        new ServoStatelessProgrammableSwitchAccessory(this, accessory);
 
         // link the accessory to your platform
         this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -1,141 +1,119 @@
+
 import { Service, PlatformAccessory, CharacteristicValue } from 'homebridge';
+import { RaspberryPiHomebridgePlatform } from './platform';
+var PythonShell = require('python-shell');
+var executePython = require("child_process").exec;
 
-import { ExampleHomebridgePlatform } from './platform';
-
-/**
- * Platform Accessory
- * An instance of this class is created for each accessory your platform registers
- * Each accessory may expose multiple services of different service types.
- */
-export class ExamplePlatformAccessory {
+export class ServoStatelessProgrammableSwitchAccessory {
+ // public readonly Service: typeof Service = this.api.hap.Service;
+  //public readonly Characteristic: typeof Characteristic = this.api.hap.Characteristic;
   private service: Service;
-
-  /**
-   * These are just used to create a working example
-   * You should implement your own code to track the state of your accessory
-   */
-  private exampleStates = {
-    On: false,
-    Brightness: 100,
-  };
+  private outputState;
+  private pythonScriptCmd;
+  private log;
+  private config;
 
   constructor(
-    private readonly platform: ExampleHomebridgePlatform,
-    private readonly accessory: PlatformAccessory,
-  ) {
+	private readonly platform: RaspberryPiHomebridgePlatform,
+	private readonly accessory: PlatformAccessory,
+  )
+  {
+      this.log = this.platform.log;
+      this.config = this.platform.config;
+      // extract name from config
+      // this.name = config.name;
+	  this.pythonScriptCmd = this.config.scriptCmd;
 
-    // set accessory information
-    this.accessory.getService(this.platform.Service.AccessoryInformation)!
-      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
-      .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
-      .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+	  // set accessory information
+	  this.accessory.getService(this.platform.Service.AccessoryInformation)!
+      .setCharacteristic(this.platform.Characteristic.Manufacturer, 'YanShon')
+      .setCharacteristic(this.platform.Characteristic.Model, 'Servo')
+      .setCharacteristic(this.platform.Characteristic.SerialNumber, '1');
 
-    // get the LightBulb service if it exists, otherwise create a new LightBulb service
-    // you can create multiple services for each accessory
-    this.service = this.accessory.getService(this.platform.Service.Lightbulb) || this.accessory.addService(this.platform.Service.Lightbulb);
+	  // you can create multiple services for each accessory
+	  this.service = this.accessory.getService(this.platform.Service.StatefulProgrammableSwitch) || this.accessory.addService(this.platform.Service.StatefulProgrammableSwitch);
 
-    // set the service name, this is what is displayed as the default name on the Home app
-    // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
-    this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.exampleDisplayName);
+	  // set the service name, this is what is displayed as the default name on the Home app
+	  // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+	  this.service.setCharacteristic(this.platform.Characteristic.Name, accessory.context.device.DisplayName);
 
-    // each service must implement at-minimum the "required characteristics" for the given service type
-    // see https://developers.homebridge.io/#/service/Lightbulb
+      // create a new Stateless Programmable Switch service
+      // this.service = new this.Service.StatefulProgrammableSwitch(this.name);
 
-    // register handlers for the On/Off Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.On)
-      .onSet(this.setOn.bind(this))                // SET - bind to the `setOn` method below
-      .onGet(this.getOn.bind(this));               // GET - bind to the `getOn` method below
+      // create handlers for required characteristics
+      this.service.getCharacteristic(this.platform.Characteristic.ProgrammableSwitchEvent)
+        .onGet(this.handleProgrammableSwitchEventGet.bind(this));
 
-    // register handlers for the Brightness Characteristic
-    this.service.getCharacteristic(this.platform.Characteristic.Brightness)
-      .onSet(this.setBrightness.bind(this));       // SET - bind to the 'setBrightness` method below
+      this.service.getCharacteristic(this.platform.Characteristic.ProgrammableSwitchOutputState)
+        .on('get', this.handleProgrammableSwitchOutputStateGet.bind(this))
+        .on('set', this.handleProgrammableSwitchOutputStateSet.bind(this));
 
-    /**
-     * Creating multiple services of the same type.
-     *
-     * To avoid "Cannot add a Service with the same UUID another Service without also defining a unique 'subtype' property." error,
-     * when creating multiple services of the same type, you need to use the following syntax to specify a name and subtype id:
-     * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
-     *
-     * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
-     */
-
-    // Example: add two "motion sensor" services to the accessory
-    const motionSensorOneService = this.accessory.getService('Motion Sensor One Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor One Name', 'YourUniqueIdentifier-1');
-
-    const motionSensorTwoService = this.accessory.getService('Motion Sensor Two Name') ||
-      this.accessory.addService(this.platform.Service.MotionSensor, 'Motion Sensor Two Name', 'YourUniqueIdentifier-2');
-
-    /**
-     * Updating characteristics values asynchronously.
-     *
-     * Example showing how to update the state of a Characteristic asynchronously instead
-     * of using the `on('get')` handlers.
-     * Here we change update the motion sensor trigger states on and off every 10 seconds
-     * the `updateCharacteristic` method.
-     *
-     */
-    let motionDetected = false;
-    setInterval(() => {
-      // EXAMPLE - inverse the trigger
-      motionDetected = !motionDetected;
-
-      // push the new value to HomeKit
-      motionSensorOneService.updateCharacteristic(this.platform.Characteristic.MotionDetected, motionDetected);
-      motionSensorTwoService.updateCharacteristic(this.platform.Characteristic.MotionDetected, !motionDetected);
-
-      this.platform.log.debug('Triggering motionSensorOneService:', motionDetected);
-      this.platform.log.debug('Triggering motionSensorTwoService:', !motionDetected);
-    }, 10000);
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, turning on a Light bulb.
+   * Handle requests to get the current value of the "Programmable Switch Event" characteristic
    */
-  async setOn(value: CharacteristicValue) {
-    // implement your own code to turn your device on/off
-    this.exampleStates.On = value as boolean;
+  handleProgrammableSwitchEventGet() {
+    this.log.debug('Triggered GET ProgrammableSwitchEvent');
 
-    this.platform.log.debug('Set Characteristic On ->', value);
+    // set this to a valid value for ProgrammableSwitchEvent
+	this.outputState = this.platform.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS;
+
+    return this.outputState;
   }
 
   /**
-   * Handle the "GET" requests from HomeKit
-   * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
-   *
-   * GET requests should return as fast as possbile. A long delay here will result in
-   * HomeKit being unresponsive and a bad user experience in general.
-   *
-   * If your device takes time to respond you should update the status of your device
-   * asynchronously instead using the `updateCharacteristic` method instead.
-
-   * @example
-   * this.service.updateCharacteristic(this.platform.Characteristic.On, true)
+   * Handle requests to get the current value of the "Programmable Switch Output State" characteristic
    */
-  async getOn(): Promise<CharacteristicValue> {
-    // implement your own code to check if the device is on
-    const isOn = this.exampleStates.On;
+  handleProgrammableSwitchOutputStateGet(callback) {
+    this.log.debug('Triggered GET ProgrammableSwitchOutputState');
 
-    this.platform.log.debug('Get Characteristic On ->', isOn);
-
-    // if you need to return an error to show the device as "Not Responding" in the Home app:
-    // throw new this.platform.api.hap.HapStatusError(this.platform.api.hap.HAPStatus.SERVICE_COMMUNICATION_FAILURE);
-
-    return isOn;
+    // set this to a valid value for ProgrammableSwitchOutputState
+	this.outputState = this.platform.Characteristic.ProgrammableSwitchEvent.SINGLE_PRESS;
+	var error = null;
+	callback(error, this.outputState);
   }
 
   /**
-   * Handle "SET" requests from HomeKit
-   * These are sent when the user changes the state of an accessory, for example, changing the Brightness
+   * Handle requests to set the "Programmable Switch Output State" characteristic
    */
-  async setBrightness(value: CharacteristicValue) {
-    // implement your own code to set the brightness
-    this.exampleStates.Brightness = value as number;
+  handleProgrammableSwitchOutputStateSet(value, callback) {
+    this.log.debug('Triggered SET ProgrammableSwitchOutputState:', value);
+	// var options = {};
+	// options.scriptPath = this.pythonScriptPath;
+			
+	// PythonShell.run(this.pythonScriptName, options, function (err, results) {
+	// 	if (err) {
+	// 		this.log.debug("Script Error", options.scriptPath, options.args, err);
+	// 		callback(err);
+	// 	} else {
+	// 		// results is an array consisting of messages collected during execution
+	// 		this.log.debug('%j', results);
+	// 	}
+	// }.bind(this));
 
-    this.platform.log.debug('Set Characteristic Brightness -> ', value);
+	// Execute command to detect state
+	this.log.debug('Set State Value:', value);
+	var success = false;
+	var scriptError = null;
+	var logger = this.log;
+	executePython(this.pythonScriptCmd, function (error, stdout, stderr) {
+		// Error detection
+		if (error && stderr) {
+			success = false;
+			logger.debug('Error in running python script:', stderr);
+			scriptError = stderr;
+		}
+
+		success = true;
+	});
+
+	if (success) {
+		callback(null);
+	}
+	else {
+		callback(scriptError);
+	}
   }
 
 }
