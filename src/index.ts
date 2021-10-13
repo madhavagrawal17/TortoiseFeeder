f40f58a58d249aaf70476bde8794743633078fea
Madhav Agrawal <maagr@microsoft.com>
2021-10-13 14:31:31 -0700

    fixed the ts lint error

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { API } from 'homebridge';
+import { API, PlatformPluginConstructor } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
 import { RaspberryPiHomebridgePlatform } from './platform';
@@ -7,5 +7,7 @@ import { RaspberryPiHomebridgePlatform } from './platform';
  * This method registers the platform with Homebridge
  */
 export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, RaspberryPiHomebridgePlatform);
+  // @ts-ignore: Known error
+  var platformPlugin: PlatformPluginConstructor = RaspberryPiHomebridgePlatform;
+  api.registerPlatform(PLATFORM_NAME, platformPlugin);
 };
